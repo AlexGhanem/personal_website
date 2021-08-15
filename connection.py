abd36f7c0da4fe2d26f1a32a14c08c051d8eaa80
Alex <alexbghanem@gmail.com>
2021-08-14 22:53:33 -0400

    changed the projects album to a multi-item  carousel

--- connection.py
+++ connection.py
@@ -4,6 +4,6 @@ from decouple import config
 
 try:
     conn = psycopg2.connect(config("DATABASE_URL"))
-    cursor=conn.cursor(cursor_factory=RealDictCursor)
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
 except:
     "failed to establish connection"
